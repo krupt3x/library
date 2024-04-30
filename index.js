b984807998e85510698656a5a10c7e2ad910b6d0
krupt3x <161846850+krupt3x@users.noreply.github.com>
2024-04-30 10:37:17 +0530

    write functions to add books to the library and view them as well

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
+const myLibrary = [];
+
 function Book(
     title, author, pages, status
-)
-{
+) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.status = status;
-    this.info = function(){
-            return `${this.title} by ${this.author}, ${this.pages}, ${this.status}`;
+    this.info = function () {
+        return `${this.title} by ${this.author}, ${this.pages}, ${this.status}`;
     }
 }
 
-const book1 = new Book("penguins", "pengy", 230, "have read");
-console.log(book1.info());
+function addBookToLibrary() {
+    const title = "bullshit"
+    const authorName = "pengy"
+    const pages = 33
+    const status = "have read."
+    myLibrary.push( new Book(title, authorName, pages, status));
+}
+
+function displayBooks(myLibrary) {
+    for (let i = 0; i <= myLibrary.length; i++) {
+        console.log(myLibrary[i]);
+    }
+}
+
+addBookToLibrary();
+displayBooks(myLibrary);
