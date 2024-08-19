de315227284f48c659bc0558b31273c525136f9d
krupt3x <161846850+krupt3x@users.noreply.github.com>
2024-08-19 09:08:33 +0530

    create function to add new books to the library(array)

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const myLibrary = [];
+
 function Book(title, author, pages, status) {
   this.title = title;
   this.author = author;
@@ -7,3 +9,24 @@ function Book(title, author, pages, status) {
     return `${this.title}, by ${this.author}, ${this.pages}, ${this.status}`;
   };
 }
+
+function addBookToLibrary() {
+  let title = prompt("Enter title: ");
+  let author = prompt("Author's name: ");
+  let pages = prompt("No. of pages: ");
+  let status = prompt("Have you read the book?");
+  myLibrary.push(new Book(title, author, pages, status));
+}
+
+const dialog = document.querySelector("dialog");
+const addBook = document.querySelector(".add-book");
+const closeBtn = document.querySelector(".close-btn");
+
+addBook.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+closeBtn.addEventListener("click", () => {
+  dialog.close();
+});
+
