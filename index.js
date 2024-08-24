b9dab7b4f6f2d6d6b3e564553b5852f035186266
krupt3x <161846850+krupt3x@users.noreply.github.com>
2024-08-24 17:53:01 +0530

    remove the modal,create a new branch for ui,create gitignore file, adjusted the directory structure

--- index.js
+++ index.js
@@ -18,15 +18,3 @@ function addBookToLibrary() {
   myLibrary.push(new Book(title, author, pages, status));
 }
 
-const dialog = document.querySelector("dialog");
-const addBook = document.querySelector(".add-book");
-const closeBtn = document.querySelector(".close-btn");
-
-addBook.addEventListener("click", () => {
-  dialog.showModal();
-});
-
-closeBtn.addEventListener("click", () => {
-  dialog.close();
-});
-
